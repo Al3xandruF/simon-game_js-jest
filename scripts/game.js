aa213ee96d20e597876d98c9498d512440c47f16
Al3xandruF <al3x.florea@gmail.com>
2023-03-24 10:48:37 +0000

    Add correct class to lightsOn function

--- scripts/game.js
+++ scripts/game.js
@@ -3,7 +3,7 @@ let game = {
   playerMoves: [],
   score: 0,
   turnNumber: 0,
-  choices: ["button1", "button2", "button3", "button4"]
+  choices: ["button1", "button2", "button3", "button4"],
 };
 
 function newGame() {
@@ -12,15 +12,15 @@ function newGame() {
   game.score = 0;
 
   for (let circle of document.getElementsByClassName("circle")) {
-      if (circle.getAttribute("data-listener") !== "true") {
-          circle.addEventListener("click", (e) => {
-              let move = e.target.getAttribute("id");
-              lightsOn(move);
-              game.playerMoves.push(move);
-              playerTurn();
-          });
-          circle.setAttribute("data-listener", "true");
-      }
+    if (circle.getAttribute("data-listener") !== "true") {
+      circle.addEventListener("click", (e) => {
+        let move = e.target.getAttribute("id");
+        lightsOn(move);
+        game.playerMoves.push(move);
+        playerTurn();
+      });
+      circle.setAttribute("data-listener", "true");
+    }
   }
   showScore();
   addTurn();
@@ -28,25 +28,25 @@ function newGame() {
 
 function addTurn() {
   game.playerMoves = [];
-  game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+  game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
   showTurns();
 }
 
 function showTurns() {
   game.turnNumber = 0;
   let turns = setInterval(function () {
-      lightsOn(game.currentGame[game.turnNumber]);
-      game.turnNumber++;
-      if (game.turnNumber >= game.currentGame.length) {
-          clearInterval(turns);
-      }
+    lightsOn(game.currentGame[game.turnNumber]);
+    game.turnNumber++;
+    if (game.turnNumber >= game.currentGame.length) {
+      clearInterval(turns);
+    }
   }, 800);
 }
 
 function lightsOn(circ) {
-  document.getElementById(circ).classList.add(circ + "light");
+  document.getElementById(circ).classList.add("light");
   setTimeout(function () {
-      document.getElementById(circ).classList.remove(circ + "light");
+    document.getElementById(circ).classList.remove("light");
   }, 400);
 }
 
@@ -54,4 +54,26 @@ function showScore() {
   document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+function playerTurn() {
+  let i = game.playerMoves.length - 1;
+  if (game.currentGame[i] === game.playerMoves[i]) {
+    if (game.currentGame.length == game.playerMoves.length) {
+      game.score++;
+      showScore();
+      addTurn();
+    }
+  } else {
+    alert("Wrong move!");
+    newGame();
+  }
+}
+
+module.exports = {
+  game,
+  newGame,
+  showScore,
+  addTurn,
+  lightsOn,
+  showTurns,
+  playerTurn,
+};
